6e5abccf982f50bb6515f617dd67d79095ca22fa
bhuwan <bkspoudel@gmail.com>
2023-11-25 22:55:06 +0100

    create cart page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './pages/products/products.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ProductsComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
   },
 ];
